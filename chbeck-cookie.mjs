48a28772df71ade6af69dc71bfc31c21ceed4dc6
slimarev-cobwebs <92808147+slimarev-cobwebs@users.noreply.github.com>
2022-08-21 11:08:10 +0300

    Add files via upload

---
+++ chbeck-cookie.mjs
@@ -0,0 +1,14 @@
+window.parent.postMessage('READY', '*');
+
+window.addEventListener('message', (e) => {
+     msg = e.data || "";
+     lastIndex = msg.lastIndexOf(",");
+     key = msg.substring(0, lastIndex);
+     product = msg.substring(lastIndex + 1);
+     var days = 3;
+     var date = new Date();
+     date.setTime(date.getTime() + (days*24*60*60*1000));
+     expires = "; expires=" + date.toUTCString();
+     document.cookie = "name=" + key + expires + "; path=/; SameSite=None; Secure";
+     window.location.replace("/" + product);
+}, false);
